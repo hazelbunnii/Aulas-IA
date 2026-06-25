29373eee3f249ab2a987c9eecbcd79f1acd3fb69
Hazel <hazelbunnii34@gmail.com>
2026-06-25 07:07:39 -0300

    Delete Aulas IA directory

--- Aulas IA/Aulas Javascript/Agentes Inteligentes/Sensor de Luz Noturna/index.js
+++
@@ -1,9 +0,0 @@
-//Percepção (Ambiente: "escuro" ou "claro")
-const ambiente = "escuro";
-
-//Regra Racional (tomada de decisão)
-if (ambiente === "escuro"){
-    console.log ("[Atuador]: Lâmpada ACESA");
-}else{
-    console.log ("[Atuador]: Lâmpada APAGADA");
-}
